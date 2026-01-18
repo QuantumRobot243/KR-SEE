8cf65b6daee8a5af785895dca6876697ff4979fb
QuantumRobot243 <aliarif1168@gmail.com>
2026-01-18 15:32:57 +0530

    seccomp error resolving

--- target/debug/build/libc-a2cc5b7faf4e561e/build_script_build-a2cc5b7faf4e561e.d
+++
@@ -1,5 +0,0 @@
-/home/miku/Desktop/Phase/KR-SEE/target/debug/build/libc-a2cc5b7faf4e561e/build_script_build-a2cc5b7faf4e561e.d: /home/miku/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.180/build.rs
-
-/home/miku/Desktop/Phase/KR-SEE/target/debug/build/libc-a2cc5b7faf4e561e/build_script_build-a2cc5b7faf4e561e: /home/miku/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.180/build.rs
-
-/home/miku/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libc-0.2.180/build.rs:
--- target/debug/build/signal-hook-fb29e9cfd21b6199/build_script_build-fb29e9cfd21b6199.d
+++
@@ -1,5 +0,0 @@
-/home/miku/Desktop/Phase/KR-SEE/target/debug/build/signal-hook-fb29e9cfd21b6199/build_script_build-fb29e9cfd21b6199.d: /home/miku/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/signal-hook-0.3.18/build.rs
-
-/home/miku/Desktop/Phase/KR-SEE/target/debug/build/signal-hook-fb29e9cfd21b6199/build_script_build-fb29e9cfd21b6199: /home/miku/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/signal-hook-0.3.18/build.rs
-
-/home/miku/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/signal-hook-0.3.18/build.rs:
